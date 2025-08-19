ae3d0d8be9345278020b3b237824cc2e6e494c7e
Gonzalo Dániel <sabius@gmail.com>
2025-08-18 22:12:08 -0500

    Adding tailwing and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    outDir: 'public',
-    publicDir: 'static',
+  outDir: 'public',
+  publicDir: 'static',
+  vite: {
+    css: {
+      transformer: 'lightningcss', // optional; you can try removing if still broken
+    },
+    plugins: [tailwindcss()],
+  },
 });
