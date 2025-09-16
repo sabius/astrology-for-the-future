cd896c921248c755c8b04b05a483e27b59ed932d
Gonzalo Dániel <sabius@gmail.com>
2025-09-15 21:46:53 -0500

    Resetting config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://sabius.github.io/astrology-for-the-future',
-  base: '/astrology-for-the-future',
+  base: process.env.BASE_PATH || '/',
   vite: {
     css: {
       transformer: 'lightningcss', // optional; you can try removing if still broken
