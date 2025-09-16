ebde588ae68c1ad666680981a4eeaf4094b30b60
Gonzalo Dániel <sabius@gmail.com>
2025-09-15 21:15:45 -0500

    Changing preview to github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://sabius.github.io/astrology-for-the-future',
-  base: process.env.BASE_URL || '/astrology-for-the-future',
+  base: '/astrology-for-the-future',
   vite: {
     css: {
       transformer: 'lightningcss', // optional; you can try removing if still broken
