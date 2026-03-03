06a5ec97d4e32b75c978648f4d07f3bf76147c0f
Gonzalo Dániel <sabius@gmail.com>
2026-03-02 22:48:27 -0500

    Fixing the index page and adding some i18n support

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,11 +12,4 @@ export default defineConfig({
     },
     plugins: [tailwindcss()],
   },
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  }
 });
